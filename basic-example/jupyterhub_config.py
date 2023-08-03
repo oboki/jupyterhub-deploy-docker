493171f252c568bcb3da2973dbeb2c8d8d832ffb
oboki <oboki@kakao.com>
2023-08-04 00:17:11 +0900

    select worker node

--- basic-example/jupyterhub_config.py
+++ basic-example/jupyterhub_config.py
@@ -1,66 +1,72 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
+# https://github.com/jupyterhub/dockerspawner/blob/main/examples/image_form/jupyterhub_config.py
 
-# Configuration file for JupyterHub
 import os
 
-c = get_config()  # noqa: F821
+c = get_config()  # noqa
 
-# We rely on environment variables to configure JupyterHub so that we
-# avoid having to rebuild the JupyterHub container every time we change a
-# configuration parameter.
 
-# Spawn single-user servers as Docker containers
-c.JupyterHub.spawner_class = "dockerspawner.DockerSpawner"
+options_form_tpl = """
+<label for="image">Image</label>
+<select name="image" size="1">
+    <option value="jupyter/base-notebook">base-notebook</option>
+    <option value="jupyter/base-notebook-2">base-notebook-2</option>
+</select>
+"""
 
-# Spawn containers from this image
-c.DockerSpawner.image = os.environ["DOCKER_NOTEBOOK_IMAGE"]
 
-# JupyterHub requires a single-user instance of the Notebook server, so we
-# default to using the `start-singleuser.sh` script included in the
-# jupyter/docker-stacks *-notebook images as the Docker run command when
-# spawning containers.  Optionally, you can override the Docker run command
-# using the DOCKER_SPAWN_CMD environment variable.
-spawn_cmd = os.environ.get("DOCKER_SPAWN_CMD", "start-singleuser.sh")
-c.DockerSpawner.cmd = spawn_cmd
+def get_options_form(spawner):
+    return options_form_tpl.format(default_image=spawner.image)
 
-# Connect containers to this Docker network
-network_name = os.environ["DOCKER_NETWORK_NAME"]
-c.DockerSpawner.use_internal_ip = True
-c.DockerSpawner.network_name = network_name
 
-# Explicitly set notebook directory because we'll be mounting a volume to it.
-# Most `jupyter/docker-stacks` *-notebook images run the Notebook server as
-# user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
-# We follow the same convention.
+c.DockerSpawner.options_form = get_options_form
+
+from dockerspawner import DockerSpawner
+
+
+class CustomDockerSpawner(DockerSpawner):
+    def options_from_form(self, formdata):
+        options = {}
+        image_form_list = formdata.get("image", [])
+        if image_form_list and image_form_list[0]:
+            options["image"] = image_form_list[0].strip()
+            self.log.info(f"User selected image: {options['image']}")
+        return options
+
+    def load_user_options(self, options):
+        image = options.get("image")
+        if image:
+            self.log.info(f"Loading image {image}")
+            self.image = image
+
+
+c.JupyterHub.spawner_class = CustomDockerSpawner
+
+# the rest of the config is testing boilerplate
+# to make the Hub connectable from the containers
+
+# dummy for testing. Don't use this in production!
+c.JupyterHub.authenticator_class = "dummy"
+# while using dummy auth, make the *public* (proxy) interface private
+c.JupyterHub.ip = "0.0.0.0"
+
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = "0.0.0.0"
+
+# may need to set hub_connect_ip to be connectable to containers
+# default hostname behavior usually works, though
+# c.JupyterHub.hub_connect_ip
+
+# pick a default image to use when none is specified
+c.DockerSpawner.image = "jupyter/base-notebook"
+
+# delete containers when they stop
+c.DockerSpawner.remove = True
+
 notebook_dir = os.environ.get("DOCKER_NOTEBOOK_DIR", "/home/jovyan/work")
 c.DockerSpawner.notebook_dir = notebook_dir
 
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
 c.DockerSpawner.volumes = {"jupyterhub-user-{username}": notebook_dir}
-
-# Remove containers once they are stopped
-c.DockerSpawner.remove = True
-
-# For debugging arguments passed to spawned containers
-c.DockerSpawner.debug = True
-
-# User containers will access hub by container name on the Docker network
-c.JupyterHub.hub_ip = "jupyterhub"
-c.JupyterHub.hub_port = 8080
-
-# Persist hub data on volume mounted inside container
-c.JupyterHub.cookie_secret_file = "/data/jupyterhub_cookie_secret"
-c.JupyterHub.db_url = "sqlite:////data/jupyterhub.sqlite"
-
-# Authenticate users with Native Authenticator
-c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
-
-# Allow anyone to sign-up without approval
-c.NativeAuthenticator.open_signup = True
-
-# Allowed admins
-admin = os.environ.get("JUPYTERHUB_ADMIN")
-if admin:
-    c.Authenticator.admin_users = [admin]
+c.DockerSpawner.use_internal_ip = True
+c.DockerSpawner.network_name = "jupyterhub-network"
